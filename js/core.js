fdd458ca03f869183794c7170898eaf589b4e598
Dhkamp <dhoelkeskamp@gmail.com>
2014-11-02 13:15:01 +0100

    mocha testing added

--- js/core.js
+++ js/core.js
@@ -1,9 +1,16 @@
 module.exports = (function () {
 
-	function ensureElement(elm) {
-
-	}
+    function convertStringToMs(str) {
+        if(str.indexOf('s') >= 0 && str.indexOf('ms') === -1) {
+            return (parseInt(str, 10) * 1000);
+        } else {
+            return parseInt(str, 10);
+        }
+    }
 
 	return {
-	}
+        Utilities: {
+            convertStringToMs: convertStringToMs
+        }
+	};
 }());
--- js/transition.js
+++ js/transition.js
@@ -1,2 +1,58 @@
-module.exports = function() {
+var Core = require('./core');
+
+module.exports = function () {
+    var _this       = this,
+        duration    = '50ms',
+        type        = 'ease',
+        delay       = '0ms',
+        property    = 'all';
+
+    this.Config = function (cfg) {
+        var i,
+            j,
+            prop;
+
+        if (arguments.length === 0) {
+            return {
+                duration    : duration,
+                type        : type,
+                delay       : delay,
+                property    : property
+            };
+        } else {
+            for (prop in cfg) {
+                switch (prop.toLowerCase()) {
+                case 'duration':
+                case 'dur':
+                    //_self.Duration(cfg[property]);
+                    break;
+                case 'type':
+                    //_self.Type(cfg[property]);
+                    break;
+                case 'delay':
+                case 'del':
+                    //_self.Delay(cfg[property]);
+                    break;
+                case 'property':
+                case 'prop':
+                    //_self.Property(cfg[property]);
+                    break;
+                }
+            }
+            return _this;
+        }
+    };
+
+    this.Delay = function (del) {
+        if (arguments.length === 0) {
+            return delay;
+        } else {
+            if (typeof del === 'string') {
+                delay = Core.Utilities.convertStringToMs(del);
+            } else {
+                delay = del + '';
+            }
+            return _this;
+        }
+    };
 };
